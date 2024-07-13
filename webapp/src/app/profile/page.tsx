797e02eca4cfd28377fb00696287c20e9b57a8e7
efe <e.sozen@student.tudelft.nl>
2024-07-14 01:24:14 +0200

    subgraph fix

--- webapp/src/app/profile/page.tsx
+++ webapp/src/app/profile/page.tsx
@@ -6,43 +6,34 @@ import { ProfileDetails } from "@/app/components/ProfileDetails";
 import { ProfileStatCard } from "../components/ProfileStatCard";
 import { convertEthToHumanReadable } from "../helpers";
 
-import {
-  DynamicWidget,
-  useSocialAccounts,
-  useUserWallets,
-} from "@dynamic-labs/sdk-react-core";
-import {
-  execute,
-  ProfileBountiesDocument,
-  ProfileDetailsDocument,
-} from "../../../.graphclient";
+import { DynamicWidget, useDynamicContext, useSocialAccounts, useUserWallets } from "@dynamic-labs/sdk-react-core";
+import { execute, ProfileBountiesDocument, ProfileDetailsDocument } from "../../../.graphclient";
 
 import { WagmiProvider } from "wagmi";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { config, queryClient } from "../config";
 
 export default function Home() {
-  const wallets = useUserWallets();
+  const { primaryWallet } = useDynamicContext();
   const { getLinkedAccountInformation } = useSocialAccounts();
   const account = getLinkedAccountInformation("farcaster" as any);
   const [participant, setParticipant] = useState<any>([]);
   const [bounty, setBounty] = useState<any[]>([]);
   useEffect(() => {
-    if (wallets && wallets.length > 0) {
-      execute(ProfileDetailsDocument, { address: wallets[0].address }).then(
-        (result) => {
-          console.log(result.data.participant);
-          setParticipant(result.data.participant);
-        }
-      );
+    const addy = primaryWallet?.address;
+    console.log("ADDY", addy);
+    if (addy) {
+      console.log("HERE", addy);
+      execute(ProfileDetailsDocument, { address: addy }).then((result) => {
+        console.log(result.data.participant);
+        setParticipant(result.data.participant);
+      });
 
-      execute(ProfileBountiesDocument, { address: wallets[0].address }).then(
-        (result) => {
-          setBounty(result.data.bounties);
-        }
-      );
+      execute(ProfileBountiesDocument, { address: addy }).then((result) => {
+        setBounty(result.data.bounties);
+      });
     }
-  }, []);
+  }, [primaryWallet]);
   return (
     <main>
       <WagmiProvider config={config}>
@@ -56,29 +47,19 @@ export default function Home() {
             <div className="grid grid-cols-2 gap-4">
               <ProfileStatCard
                 name="Total Rewards"
-                value={`${convertEthToHumanReadable(
-                  participant.totalRewards
-                )} ETH`}
+                value={`${convertEthToHumanReadable(participant.totalRewards)} ETH`}
               />
               {participant.participated && (
-                <ProfileStatCard
-                  name="Bounties Completed"
-                  value={participant.participated.length}
-                />
+                <ProfileStatCard name="Bounties Completed" value={participant.participated.length} />
               )}
-              <ProfileStatCard
-                name="Bounties Offered"
-                value={bounty.length.toString()}
-              />
+              <ProfileStatCard name="Bounties Offered" value={bounty.length.toString()} />
             </div>
           )}
           <div className="flex justify-between font-bold text-xl pt-12">
             <p>My Bounties</p>
           </div>
           {bounty.length > 0 ? (
-            bounty.map((bounty) => (
-              <BountyCard key={bounty.id} bounty={bounty} />
-            ))
+            bounty.map((bounty) => <BountyCard key={bounty.id} bounty={bounty} />)
           ) : (
             <div>Loading...</div>
           )}
